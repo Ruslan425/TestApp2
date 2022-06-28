4553c861f2a841e5b82f3af3475ebcea5296816a
Ruslan425 <92014369+Ruslan425@users.noreply.github.com>
2022-06-29 01:35:59 +0300

    Задание №2 0.1

---
+++ app/src/main/java/ru/romazanov/java/Arrays.java
@@ -0,0 +1,17 @@
+package ru.romazanov.java;
+
+public class Arrays {
+
+    public static String array() {
+        int[] arr = {1,2,3,4,5};
+        int[][] arr2 = {{1,2,3},{1,2,3}};
+        int[][][] arr3 = {{{1,2,3}, {1,2,3}},{{1,2,3}, {1,2,3}}};
+
+        int[][] arr4 = new int [2][2]; // Регулярный
+        int[][][] arr5 = new int [2][][]; // Нерегулярный
+
+       return String.valueOf(arr3[0][1][1]);
+
+    }
+
+}
